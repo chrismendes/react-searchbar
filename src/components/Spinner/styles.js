8fe5907031c538cf54bdcbbe743b6a27ff1ef3c5
Chris Mendes <chris.m@clicktech.com>
2022-12-17 23:17:37 +0000

    Added spinner component

---
+++ src/components/Spinner/styles.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+// Courtesy of: https://projects.lukehaas.me/css-loaders/
+
+export const Spinner = styled.div`
+  color: #666;
+  font-size: 10px;
+  margin: 0 auto 40px auto;
+  position: relative;
+  text-indent: -9999em;
+  transform: translateZ(0);
+  animation-delay: -0.16s;
+
+  &,
+  &:before,
+  &:after {
+    border-radius: 50%;
+    width: 12px;
+    height: 12px;
+    animation-fill-mode: both;
+    animation: load7 1.8s infinite ease-in-out;
+  }
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    top: 0;
+  }
+  &:before {
+    left: -20px;
+    animation-delay: -0.32s;
+  }
+  &:after {
+    left: 20px;
+  }
+  @-webkit-keyframes load7 {
+    0%,
+    80%,
+    100% {
+      box-shadow: 0 2.5em 0 -1.3em;
+    }
+    40% {
+      box-shadow: 0 2.5em 0 0;
+    }
+  }
+  @keyframes load7 {
+    0%,
+    80%,
+    100% {
+      box-shadow: 0 2.5em 0 -1.3em;
+    }
+    40% {
+      box-shadow: 0 2.5em 0 0;
+    }
+  }
+`;
