69856a7632259e8d487808a80aecef9e0fa2538a
Chris Mendes <chris.m@clicktech.com>
2022-12-20 00:31:06 +0000

    Removed uneeded layout files from first version app layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,4 +1,1 @@
 export { default as LayoutSiteHeader } from './LayoutSiteHeader';
-export { default as LayoutBody } from './LayoutBody';
-export { default as LayoutSiteFooter } from './LayoutSiteFooter';
-export { default as LayoutContainer } from './LayoutContainer';
